4f993abd187749eca51842cbefd6c64809df9b83
HoseungJang <hsjang.dev@gmail.com>
2021-05-05 03:33:51 +0900

    add github service

---
+++ src/services/github.ts
@@ -0,0 +1,24 @@
+import { Octokit } from "@octokit/rest";
+import { decode } from "js-base64";
+
+export class Github {
+  private static readonly instance = new Octokit({
+    auth: process.env.REACT_APP_GITHUB_TOKEN,
+    baseUrl: "https://api.github.com",
+  });
+
+  private static readonly owner = "HoseungJang";
+  private static readonly repo = "wiki";
+
+  public static async getContent(path: string) {
+    const { content } = (
+      await this.instance.repos.getContent({
+        owner: this.owner,
+        repo: this.repo,
+        path,
+      })
+    ).data as { content: string };
+
+    return decode(content);
+  }
+}
