cf2acd262395c2d95fc9fe8fa2681b21bec18d67
HoseungJang <hsjang.dev@gmail.com>
2021-05-05 03:42:32 +0900

    add /not-found route

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import { render } from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 
-import { Document } from "./pages/Document";
 import { ScrollToTop } from "./ScrollToTop";
+import { NotFound } from "./pages/NotFound";
+import { Document } from "./pages/Document";
 
 render(
   <BrowserRouter>
     <ScrollToTop />
+    <Route path="/not-found" component={NotFound} />
     <Route path="*" component={Document} />
   </BrowserRouter>,
   document.getElementById("root")
