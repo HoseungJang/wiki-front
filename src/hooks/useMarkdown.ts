48845c042caa1b4fe4c9b88f482141f305b72aba
HoseungJang <hsjang.dev@gmail.com>
2021-05-05 03:43:08 +0900

    redirect to /not-found when error occured from Github service

--- src/hooks/useMarkdown.ts
+++ src/hooks/useMarkdown.ts
@@ -1,13 +1,21 @@
-import { Github } from "../service/github";
+import { useHistory } from "react-router";
 import useSWR from "swr";
 
+import { Github } from "../services/github";
+
 export function useMarkdown() {
+  const history = useHistory();
+
   const { data } = useSWR(
     decodeURIComponent(window.location.pathname.slice(1)) || "README.md",
     async (path: string) => {
-      return await Github.getContent(path);
+      try {
+        return await Github.getContent(path);
+      } catch (error) {
+        history.push("/not-found");
+      }
     },
-    { dedupingInterval: 3600 * 60 }
+    { dedupingInterval: 3600 * 60, shouldRetryOnError: false }
   );
 
   return data ?? null;
