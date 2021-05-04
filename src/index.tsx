77a436d8e38cd49f9542ba96453421c943e16338
HoseungJang <hsjang.dev@gmail.com>
2021-05-05 04:02:21 +0900

    apply global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import { render } from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 import { ScrollToTop } from "./ScrollToTop";
 import { NotFound } from "./pages/NotFound";
 import { Document } from "./pages/Document";
 
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    width: 100%;
+    height: 100%;
+
+    margin: 0;
+    padding: 0;
+  }
+`;
+
 render(
-  <BrowserRouter>
-    <ScrollToTop />
-    <Route path="/not-found" component={NotFound} />
-    <Route path="*" component={Document} />
-  </BrowserRouter>,
+  <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <ScrollToTop />
+      <Route exact path="/not-found" component={NotFound} />
+      <Route component={Document} />
+    </BrowserRouter>
+  </>,
   document.getElementById("root")
 );
