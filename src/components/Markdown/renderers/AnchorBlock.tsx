7cf3a8fad25dc1da923bb84f331a8c034b8a167b
HoseungJang <hsjang.dev@gmail.com>
2021-05-06 04:38:39 +0900

    use as

--- src/components/Markdown/renderers/AnchorBlock.tsx
+++ src/components/Markdown/renderers/AnchorBlock.tsx
@@ -5,40 +5,24 @@ import { Color } from "../../../constants/color";
 
 export function AnchorBlock(props: any) {
   return props.href.startsWith("#") ? (
-    <S.Hash href={props.href}>{props.children}</S.Hash>
+    <S.Container href={props.href}>{props.children}</S.Container>
   ) : props.href.startsWith("http") ? (
-    <S.Anchor href={props.href}>{props.children}</S.Anchor>
+    <S.Container href={props.href}>{props.children}</S.Container>
   ) : (
-    <S.Link to={props.href}>{props.children}</S.Link>
+    <S.Container as={Link} to={props.href}>
+      {props.children}
+    </S.Container>
   );
 }
 
 const S = {
-  Anchor: styled.a`
+  Container: styled.a`
     font-weight: bold;
     color: ${Color.Blue300};
     text-decoration: none;
 
     &:hover {
       color: ${Color.Blue200};
-    }
-  `,
-  Link: styled(Link)`
-    font-weight: bold;
-    color: ${Color.Blue300};
-    text-decoration: none;
-
-    &:hover {
-      color: ${Color.Blue200};
-    }
-  `,
-  Hash: styled.a`
-    font-weight: bold;
-    color: ${Color.Purple300};
-    text-decoration: none;
-
-    &:hover {
-      color: ${Color.Purple200};
     }
   `,
 };
