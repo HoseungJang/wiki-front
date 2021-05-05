4e13a7e6e8c6d0581c5293f0e7579ec4cd3772b1
HoseungJang <hsjang.dev@gmail.com>
2021-05-06 05:22:02 +0900

    add TableBlock

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -5,6 +5,7 @@ import slug from "remark-slug";
 import { AnchorBlock } from "./renderers/AnchorBlock";
 import { ImageBlock } from "./renderers/ImageBlock";
 import { CodeBlock } from "./renderers/CodeBlock";
+import { TableBlock } from "./renderers/TableBlock";
 
 export function Markdown({ content }: { content: string }) {
   return (
@@ -14,6 +15,7 @@ export function Markdown({ content }: { content: string }) {
         a: AnchorBlock,
         img: ImageBlock,
         code: CodeBlock,
+        table: TableBlock,
       }}
     >
       {content}
---
+++ src/components/Markdown/renderers/TableBlock.tsx
@@ -0,0 +1,53 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+import { Color } from "../../../constants/color";
+
+import { useTheme, Theme } from "../../../contexts/Theme";
+
+export function TableBlock({ children }: { children: ReactNode }) {
+  const { currentTheme } = useTheme();
+
+  return <S.Container t={currentTheme}>{children}</S.Container>;
+}
+
+const S = {
+  Container: styled.table<{ t: Theme }>`
+    border-spacing: 0;
+
+    > thead {
+      > tr {
+        > th {
+          padding: 6px 13px;
+
+          box-sizing: border-box;
+
+          border: 1px solid
+            ${({ t }) => ({ light: Color.Grey200, dark: Color.Black50 }[t])};
+
+          &:not(:last-child) {
+            border-right: none;
+          }
+        }
+      }
+    }
+
+    > tbody {
+      > tr {
+        > td {
+          padding: 6px 13px;
+
+          box-sizing: border-box;
+
+          border: 1px solid
+            ${({ t }) => ({ light: Color.Grey200, dark: Color.Black50 }[t])};
+          border-top: none;
+
+          &:not(:last-child) {
+            border-right: none;
+          }
+        }
+      }
+    }
+  `,
+};
