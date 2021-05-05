55a7839fd43cc2e24b4942ce0d1c6e952698b501
HoseungJang <hsjang.dev@gmail.com>
2021-05-06 04:52:11 +0900

    apply styles

--- src/components/Markdown/renderers/ImageBlock.tsx
+++ src/components/Markdown/renderers/ImageBlock.tsx
@@ -1,5 +1,15 @@
+import styled from "styled-components";
+
 import { Github } from "../../../services/github";
 
 export function ImageBlock(props: any) {
-  return <img src={Github.getContentURL(props.src)} alt="" />;
+  return <S.Container src={Github.getContentURL(props.src)} alt="" />;
 }
+
+export const S = {
+  Container: styled.img`
+    max-width: 100%;
+
+    object-fit: contain;
+  `,
+};
