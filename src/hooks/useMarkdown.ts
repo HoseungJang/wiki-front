cf90b07ff4ac8d39d7c1d80d2c9ca266a55451c0
HoseungJang <hsjang.dev@gmail.com>
2021-05-06 04:34:53 +0900

    apply resetting hash for scrolling

--- src/hooks/useMarkdown.ts
+++ src/hooks/useMarkdown.ts
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router";
 import useSWR from "swr";
 
@@ -17,6 +18,13 @@ export function useMarkdown(path: string) {
     },
     { dedupingInterval: 3600 * 60, shouldRetryOnError: false }
   );
+
+  useEffect(() => {
+    if (history.location.hash !== "" && data) {
+      window.location.hash = "";
+      window.location.hash = history.location.hash;
+    }
+  }, [history, data]);
 
   return data ?? null;
 }
