a112f46ddcb301566ea245f731a1c9e5598b2e0c
HoseungJang <hsjang.dev@gmail.com>
2021-05-05 17:31:39 +0900

    update global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import { render } from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 
+import { Color } from "./constants/color";
+
 import { ScrollToTop } from "./ScrollToTop";
 import { NotFound } from "./pages/NotFound";
 import { Document } from "./pages/Document";
@@ -13,6 +15,17 @@ const GlobalStyle = createGlobalStyle`
 
     margin: 0;
     padding: 0;
+
+    background-color: ${Color.Grey50};
+
+    > #root {
+      width: 100%;
+      height: 100%;
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
   }
 `;
 
